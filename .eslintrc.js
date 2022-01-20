35297ee4645f96e371ed230331c2c7a9136fea46
SakuraCCM <1005864372@qq.com>
2022-01-20 19:03:17 +0800

    项目结构搭建完成

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,14 @@ module.exports = {
     node: true
   },
   extends: [
+    // vue官方的eslint插件
     'plugin:vue/essential',
     '@vue/standard'
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
+  // 代码风格校验代码，修改配置文件需要重启服务器
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
+// 加载vueRouter,注册为vue插件
 Vue.use(VueRouter)
 
+// 路由规则
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    // 路由地址
+    path: '/home',
+    name: 'home',
+    // 路由组件
+    component: () => import(/* webpackChunkName: 'home' */'@/views/home/index')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/',
+    component: () => import(/* webpackChunkName: 'layout' */'@/views/layout/index'),
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: () => import(/* webpackChunkName: 'home' */'@/views/home/index')
+      }
+      // 书写后续内部的路由组件
+    ]
+  },
+  {
+    path: '*',
+    name: 'error-page',
+    // 路由优化：路由懒加载
+    component: () => import(/* webpackChunkName: 'error-page' */'@/views/error-page/index')
   }
 ]
 
